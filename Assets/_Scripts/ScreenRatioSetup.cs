4ad815950fce57420ef5e4a1b48ed5beeacbaed7
Alex <109491531+ilexl@users.noreply.github.com>
2023-07-26 14:26:35 +1200

    Settings-Window-Graphics

---
+++ Assets/_Scripts/ScreenRatioSetup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRatioSetup : MonoBehaviour
+{
+    public bool fullScreen = true;  // Whether the game should be in fullscreen mode
+#if UNITY_EDITOR
+#else
+    private void Start()
+    {
+        // Calculate the target height based on the screen width and 16:9 aspect ratio
+        int targetHeight = Screen.width * 9 / 16;
+
+        // Set the game's resolution to match the target width and height
+        Screen.SetResolution(Screen.width, targetHeight, fullScreen);
+    }
+#endif
+}
